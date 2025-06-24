398ff8da4ec580def98ba1c87306ea3fef9ffd7d
Saikat Ghosh <saikat.blast20@gmail.com>
2025-06-24 17:48:39 +0600

    responseLearning

--- first.js
+++ first.js
@@ -32,16 +32,32 @@
 
 // Understanding request Object 
 
-const http = require("http");
-const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers)
-});
+// const http = require("http");
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url, req.method, req.headers)
+// });
 
-const PORT = 3000;
-server.listen(PORT, ()=> {
-     console.log(`Server running at http://localhost:${PORT}`)
-})
+// const PORT = 3000;
+// server.listen(PORT, ()=> {
+//      console.log(`Server running at http://localhost:${PORT}`)
+// })
 
 
 
 // next one how to send response. (cosider like a shopping bag. )
+
+const http = require("http");
+const server = http.createServer((req, res)=>{
+     res.setHeader('content-Type', 'text/html');
+     res.write('<html>');
+     res.write('<head><title>Saikat coding</title></head>');
+     res.write('<body><h1>This is heading</h1><body>');
+     res.write('</html>');
+     res.end();
+
+});
+
+const PORT = 3000;
+server.listen(PORT, ()=>{
+    console.log(`Server running at http://localhost:${PORT}`);
+});
